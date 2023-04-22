0ab96c0cd4c840cb957a9a27c749345fbf9eede6
ammernico <Nico-Steinle@t-online.de>
2023-04-22 16:28:02 +0200

    Ask the user again on bad input

--- src/main.rs
+++ src/main.rs
@@ -6,32 +6,38 @@ fn calc_bmi(weight: f32, height: f32) -> f32 {
     weight / (height * height)
 }
 
-fn take_user_input() -> f32 {
-    let mut input_line = String::new();
-    stdin()
-    .read_line(&mut input_line)
-    .expect("Failed to read line");
+fn take_user_input(value: &str) -> f32 {
+    let r: f32;
+    loop {
+        print!("Please enter the {}: ", value);
+        let _ = std::io::stdout().flush();
 
-    let r: f32 = input_line
-        .trim()  // removes whitespaces
-        .parse()
-        .expect("Not a valid float");
+        let mut input_line = String::new();
+        stdin()
+        .read_line(&mut input_line)
+        .expect("Failed to read line");
+
+        let input = input_line
+            .trim()  // removes whitespaces
+            .parse();
+        
+        match input {
+            Ok(input) => {
+                r = input;
+                break;
+            }
+            _ => {
+                println!("bad input")
+            }
+        }
+    }
     r
 }
 
-fn terminal_user_bmi() {
-    print!("Please enter the weight in kilograms: ");
-    let _ = std::io::stdout().flush();
-    let weight: f32 = take_user_input();
-
-    print!("Please enter the height in meters : ");
-    let _ = std::io::stdout().flush();
-    let height: f32 = take_user_input();
+fn main() {
+    let weight = take_user_input("weight in kilograms");
+    let height = take_user_input("height in meters");
 
     let bmi = calc_bmi(weight, height);
     println!("BMI is {}", bmi);
-}
-
-fn main() {
-    terminal_user_bmi()
 }
