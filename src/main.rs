929a3bde10cf9f9e8d705a5127b2dfa5c70b5322
ammernico <Nico-Steinle@t-online.de>
2023-05-06 16:58:22 +0200

    Add basic testing for calc_bmi

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,24 @@ struct Weight(f32);
 struct Height(f32);
 struct Bmi(f32);
 
-fn calc_bmi(w: Weight, h: Height) -> Bmi {
+fn calc_bmi(w: Weight, h: Height) -> Option<Bmi> {
+    if h.0 <= 0.0 {
+        return None;
+    }
     // weight (kg) / height**2 (m)
-    Bmi(w.0 / (h.0 * h.0))
+    Some(Bmi(w.0 / (h.0 * h.0)))
+}
+
+#[test]
+fn test_calculate_bmi() {
+    let r = calc_bmi(Weight(69.0), Height(1.69)).unwrap();
+    assert_eq!(r.0, 24.158817);
+}
+
+#[test]
+fn test_calculate_bmi_failing() {
+    let r = calc_bmi(Weight(69.0), Height(-0.0));
+    assert!(r.is_none());
 }
 
 fn take_user_input(value: &str) -> f32 {
@@ -44,6 +59,6 @@ fn main() {
     let height = take_user_input("height in meters");
     let height = Height(height);
 
-    let bmi = calc_bmi(weight, height);
+    let bmi = calc_bmi(weight, height).unwrap();
     println!("BMI is {}", bmi.0);
 }
